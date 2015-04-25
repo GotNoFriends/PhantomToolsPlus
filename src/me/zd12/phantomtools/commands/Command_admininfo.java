f3d8e3bfb89311cb0f93e13c00ad5b1390b2e3c2
Revalation <hentscheljr1@aol.com>
2015-04-24 21:05:54 -0400

    Update Command_admininfo.java

--- src/me/zd12/phantomtools/commands/Command_admininfo.java
+++ src/me/zd12/phantomtools/commands/Command_admininfo.java
@@ -2,15 +2,18 @@ package me.zd12.phantomtools.commands;
 
 import me.zd12.phantomtools.messages;
 import net.pravian.bukkitlib.command.BukkitCommand;
+import net.pravian.bukkitlib.command.CommandPermissions;
+import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+@CommandPermissions(source = SourceType.ANY, permission = "phantom.user.admininfo")
 public class Command_admininfo extends BukkitCommand {   
 
     @Override
     public boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        sender.sendMessage(messages.lulzname + "Hi there!  You may obtain administrator by applying on our forums!");
-        sender.sendMessage(ChatColor.GOLD + "http://lulzcraft.boards.net/");
+        sender.sendMessage(messages.sname + messages.ai_message); // Change the servers name and message at '.\src\me\zd12\phantomtools\messages.java'
+        sender.sendMessage(ChatColor.AQUA + "However, stay updated to see when our forums are created!:");
         return true;
     }
 }
