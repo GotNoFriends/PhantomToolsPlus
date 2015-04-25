385d0940a4bd9a82c2e501e82e7239e6b2b9c987
John J. Miller <zD12@users.noreply.github.com>
2015-04-24 21:17:16 -0400

    Update messages.java

--- src/me/zd12/phantomtools/messages.java
+++ src/me/zd12/phantomtools/messages.java
@@ -3,10 +3,17 @@ package me.zd12.phantomtools;
 import org.bukkit.ChatColor;
 
 public class messages {
-    public static final String pname = ChatColor.GRAY + "[" + ChatColor.AQUA + "Phantom" + ChatColor.LIGHT_PURPLE + "tools" + ChatColor.GREEN + "+" + ChatColor.GRAY + "] ";
-    public static final String lulzname = ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Lulz" + ChatColor.RED + "Craft" + ChatColor.GRAY + "] ";
-    public static final String notch = ChatColor.WHITE + "<" + ChatColor.GOLD + "Notch" + ChatColor.WHITE + "> ";
+    // Developer information
+    public static final String lastmodified = "April 24, 2015"; // Please only change when an active build is released.
     public static final String pver = "0.1";
-    public static final String pauthors = "zD12, Revalation, xDestroyer217";
+    public static final String pauthors = "zD12. Revalation, xDestroyer217";
+    public static final String humarerror = "https://hp.vexservers.net/misc/pt/hm/";
+    // Plugin information
+    public static final String pname = ChatColor.GRAY + "[" + ChatColor.AQUA + "Phantom" + ChatColor.LIGHT_PURPLE + "tools" + ChatColor.GREEN + "+" + ChatColor.GRAY + "] ";
+    public static final String sname = ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Lulz" + ChatColor.RED + "Craft" + ChatColor.GRAY + "] ";
+    // Links
     public static final String plink = "https://hp.vexservers.net/misc/phantomtools/";
+    public static final String forums = "https://hp.vexservers.net/misc/pt/hm/";
+    // Raw messages (spammy) (Customize)
+    public static final String ai_message = "Applying for ranks is currently impossible.  Staff members are hand-picked, due to this, applying for ranks is impossible.  Sorry.";
 }
